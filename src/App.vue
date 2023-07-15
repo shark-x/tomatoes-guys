4e0a88004acab27d74658acfe3db955e721dd7cf
Nastia K <a.karpeeva@pikabu.ru>
2023-07-15 18:22:35 +0500

    app template of app

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<template>
+  <Header/>
+</template>
+
+<script setup>
+import "agnostic-vue/dist/index.css";
+import 'agnostic-vue/dist/common.min.css'
+import Header from '@/components/Header.vue'
+</script>
---
+++ src/components/Header.vue
@@ -0,0 +1,25 @@
+<script setup>
+import "agnostic-vue/dist/common.min.css";
+import "agnostic-vue/dist/index.css";
+import { Header, HeaderNav, HeaderNavItem } from "agnostic-vue";
+</script>
+<template>
+  <Header isHeaderContentStart>
+    <template v-slot:headernav>
+      <HeaderNav css="header-flex-fill">
+        <HeaderNavItem>
+          <a href="">Tomatoes</a>
+        </HeaderNavItem>
+        <HeaderNavItem>
+          <a href="">Projects</a>
+        </HeaderNavItem>
+        <HeaderNavItem>
+          <a href="">Scores</a>
+        </HeaderNavItem>
+        <HeaderNavItem>
+          <a href="">Settings</a>
+        </HeaderNavItem>
+      </HeaderNav>
+    </template>
+  </Header>
+</template>
